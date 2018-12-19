f3974eefb410b350531d3c41453638c99840a63c
Maarten <ikmaarten@gmail.com>
2018-12-19 14:29:50 +0100

    faking ossom

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import platform
+import datetime
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hatsikidee!"
+    res = '''
+    date: %s
+    version: %s
+    platform: %s
+    uname: %s
+    processor: %s
+    node: %s''' % (
+        datetime.datetime.now(),
+        platform.version(),
+        platform.platform(),
+        platform.uname(),
+        platform.processor(),
+        platform.node(),
+    )
+    return res
 
 if __name__ == "__main__":
     application.run()
